b685b0756304bcf37328209ad59be315cf664f29
Ruturaj-Raut-27 <ruturajraut27@gmail.com>
2025-01-24 09:19:08 +0530

    Add files via upload

---
+++ HTTPMethods/SpringBootMethods/src/main/java/com/tka/SpringBootMethodsApplication.java
@@ -0,0 +1,14 @@
+package com.tka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMethodsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMethodsApplication.class, args);
+		System.out.println("APPLICATION STARTED");
+	}
+
+}
